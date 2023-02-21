67bb7ef31c9181c74d909b66acf01f66c94b0ac7
Lucas <lucas.115@hotmail.com>
2023-02-21 16:25:28 +0100

    Add direct solve functionality

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Program
 
-To write my first Python code, this is a pathfinding approach to solve the Rubik's cube. Two pathfinding approaches:
+To write my first Python code, this is a pathfinding approach to solve the Rubik's cube. Three pathfinding approaches:
 
 The first approach is a breadth-first search that uses a "visited" logic to avoid adding nodes already in the queue. Notice that the solutionness of a state is checked right before adding it to the list, as supposed to when popped from it. This makes finding the solution way faster, even if the time to explore the whole space (around 40 seconds on my pc) is about 10% slower, due to the "next" lists being longer.  
 Another way to speed it up about another 10% would be to get rid of the per-depth-level logic and just add items to the list till it's empty. The data types are correct, so it's only about reducing the amount of O(1) operations.
@@ -9,8 +9,10 @@ The second is an iterative depth approach, that doesn't necessarily find the bes
 The reason why it doesn't find the best solution is because it uses the visited logic, but it doesn't know if a solution is the shortest. Think about how there could be a faster path to any of the intermediate states of the found path. Getting rid of the visited logic would allow the algorithm to find the best (and every other) one, but it could take a while, even after cutting off some branches.
 Another interesting point is that no iteration of this version (any that Python can handle) explores the whole net, but the visited logic makes it visit a different set of nodes in each one. This is why it is important to check if every state is the solution and not just the deepest ones.
 
-I have tested A LOT of versions and data structures, non of them using heuristics, since I don't know enough about the problem to apply any, even if doing so would be rather easy with a priority queue.  
-Finally, it's also worth noting that while the absolute map does change, the relative one does not. The cube is always the cube. You could make a map of the states and navigate them in the most efficient way. The net always looks the same. You could write a table relating every relative state and the shortest path to get to it. You could then have those paths ordered and just follow them from a particular scramble and that would probably be the best way to find the solution.
+The third is a hash function-based direct solve. The first time it is called, it fills a dictionary (that could instead be imported from a file or some other faster way) using the BFS algorithm (giving this modularity to it makes the BFS very slightly slower). The dictionary relates the solved state with every other, returning its shortest (or one of all its shortest) path. So the algorithm gets this path giving the scrambled state as an argument, and just inverses it to find the shortest path from it to the solved state.
+
+
+I have tested A LOT of versions and data structures, non of them using heuristics, since I don't know enough about the problem to apply any, even if doing so would be rather easy with a priority queue.
 
 When the cube class is imported its static method fills the dictionary with the key to orientation handling when turning. I tried to extend this idea to make the turn function direct but I wasn't able to easily fill the dictionary with the 3.6 million * 9 entrances. Since I didn't even know if those many entrances are hashable, or worth hashing, I gave up on that.  
 In general, the implementation might not be the best nor Python the most adequate language, but even if it was twice as good it would only take half the time. There are 3.6 million states to be visited after all.
--- src/solver.py
+++ src/solver.py
@@ -5,6 +5,7 @@ from cube import Cube
 MOVE_KEY = {0 : 'U', 1 : 'F', 2 : 'R', 'U' : 0, 'F' : 1, 'R' : 2}
 ITERATOR_KEY = {0 : ' ', 1 : '2', 2 : '\'', ' ' : 0, '2' : 1, '\'' : 2}
 SCRAMBLE_LENGTH = 15
+DICT = {}
 
 def generateScramble(l):
     aux = ""
@@ -15,37 +16,21 @@ def generateScramble(l):
         aux = aux + MOVE_KEY[moveSeed] + ITERATOR_KEY[iteratorSeed] + " "
     return aux
 
+def getScramble():
+    if ask("Mode?\n" + "1 - Autogenerate\n" + "2 - Paste") == "2":
+        scramble = ask("Input scramble")
+    else:
+        scramble = generateScramble(SCRAMBLE_LENGTH)
+    print("scramble: " + scramble)
+    return scramble
+    
 def mix(cube, scramble):
     for i in range(SCRAMBLE_LENGTH):
         move = (scramble[3 * i : 3 * i + 2])
         for j in range(ITERATOR_KEY[move[1]] + 1):
             cube.turn(MOVE_KEY[move[0]])
-
-def solveBFS(state):
-    visited = set(tuple(state))
-    aux = Cube()
-    next = [("", state)]
-    for depth in range(1, 12):
-        print("depth: " + str(depth))
-        current = list(next)
-        next = list([])
-        for i in range(len(current)):
-            auxState = current[i]
-            last = MOVE_KEY[auxState[0][-3]] if auxState[0] else -1
-            for j in range(3):
-                if last != j:
-                    aux.setState(auxState[1])
-                    for k in range(3):
-                        aux.turn(j)
-                        if aux.isSolved():
-                            print ("solution: " + auxState[0] + MOVE_KEY[j] + ITERATOR_KEY[k])
-                            print ("visited states: " + str(len(visited)))
-                            return
-                        if not tuple(aux.getState()) in visited:
-                            visited.add(tuple(aux.getState()))
-                            next.append([auxState[0] + MOVE_KEY[j] + ITERATOR_KEY[k] + " ", aux.getState()])
-
-def solveIDS(state):
+                                
+def solveDFS(state):
     def search(state, solution, depth, last, visited):
         aux = Cube()
         for i in range(3):
@@ -72,28 +57,63 @@ def solveIDS(state):
         found = search(state, "", depth, -1, visited)
         depth += 1
 
-def solve(state):
-    if ask("Mode?\n" + "1 - DFS\n" + "2 - BFS") == "2":
-        sTime = time.time()
-        solveBFS(state)
-    else:
-        sTime = time.time()
-        solveIDS(state)
-    print("Time elapsed: {:.2f} seconds".format(time.time()-sTime))
+def solveBFS(state, mode):
+    visited = set(tuple(state))
+    aux = Cube()
+    next = [("", state)]
+    for depth in range(1, 12):
+        print("depth: " + str(depth))
+        current = list(next)
+        next = list([])
+        for i in range(len(current)):
+            auxState = current[i]
+            last = MOVE_KEY[auxState[0][-3]] if auxState[0] else -1
+            for j in range(3):
+                if last != j:
+                    aux.setState(auxState[1])
+                    for k in range(3):
+                        aux.turn(j)
+                        if mode == '1':
+                            if aux.isSolved():
+                                print ("solution: " + auxState[0] + MOVE_KEY[j] + ITERATOR_KEY[k])
+                                print ("visited states: " + str(len(visited)))
+                                return
+                            if not tuple(aux.getState()) in visited:
+                                visited.add(tuple(aux.getState()))
+                                next.append([auxState[0] + MOVE_KEY[j] + ITERATOR_KEY[k] + " ", aux.getState()])
+                        else:
+                            if not tuple(aux.getState()) in DICT:
+                                DICT[tuple(aux.getState())]= auxState[0] + MOVE_KEY[j] + ITERATOR_KEY[k] + " "
+                                next.append([auxState[0] + MOVE_KEY[j] + ITERATOR_KEY[k] + " ", aux.getState()])
 
-def getScramble():
-    if ask("Mode?\n" + "1 - Autogenerate\n" + "2 - Paste") == "2":
-        return ask("Input scramble")
+def solveDirect(state):
+    def inverseSequence(sequence):
+        inverses = {"U": "U'", "U'": "U", "U2" : "U2", "F": "F'", "F'": "F", "F2" : "F2", "R": "R'", "R'": "R", "R2" : "R2"}
+        moves = sequence.split()
+        return " ".join([inverses[move] for move in reversed(moves)])
+    if len(DICT) == 0:
+        print("Filling dictionary")
+        solveBFS([0, 3, 6, 9 ,12 ,15, 18, 21], '2')
+    print(inverseSequence(DICT[tuple(cube.getState())]))
+
+def solve(state):
+    o = ask("Mode?\n" + "1 - Direct\n" + "2 - BFS\n" + "3 - DFS")
+    sTime = time.time()
+    if o == '2':
+        solveBFS(state, '1')
+    elif o == '3':
+        solveDFS(state)
     else:
-        return generateScramble(SCRAMBLE_LENGTH)
+        solveDirect(state)
+    print("Time elapsed: {:.2f} seconds".format(time.time()-sTime))
 
 def ask (s):
     print(s)
     return input()
 
 if __name__ == '__main__':
-    cube = Cube()
-    scramble = getScramble()
-    print("scramble: " + scramble)
-    mix (cube, scramble)
-    solve(cube.getState())
+    while True:
+        cube = Cube()
+        scramble = getScramble()
+        mix (cube, scramble)
+        solve(cube.getState())
